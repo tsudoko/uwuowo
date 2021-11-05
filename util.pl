0150cebbc3b82ce78a4d701e1c98f0d5253bec89
flan <flan@flande.re>
2021-11-05 19:13:04 +0100

    Break up definition of random/2 into separate lines

--- util.pl
+++ util.pl
@@ -1,4 +1,6 @@
-random_int(From, To, X) :- random(Xf), X is floor(Xf * To) + From.
+random_int(From, To, X) :-
+	random(Xf),
+	X is floor(Xf * To) + From.
 
 pair_add(X1-Y1, X2-Y2, OX-OY) :-
 	OX is X1+X2,
