8c4d22abf078a119d614062a8dfcef777a58b0de
flan <flan@flande.re>
2021-11-11 07:38:04 +0100

    Don't assume dynamic/1 and multifile/1 are operators

--- basedef.pl
+++ basedef.pl
@@ -1,30 +1,30 @@
-:- dynamic location/2.
-:- multifile location/2.
-:- dynamic stat/3.
-:- multifile stat/3.
-:- dynamic ascii_graphic/2.
-:- multifile ascii_graphic/2.
-:- dynamic room/2.
-:- multifile room/2.
-:- dynamic sight_range/2.
-:- multifile sight_range/2.
-:- dynamic inventory/2.
-:- multifile inventory/2.
-:- dynamic btree_maze/4.
-:- multifile btree_maze/4.
+:- dynamic(location/2).
+:- multifile(location/2).
+:- dynamic(stat/3).
+:- multifile(stat/3).
+:- dynamic(ascii_graphic/2).
+:- multifile(ascii_graphic/2).
+:- dynamic(room/2).
+:- multifile(room/2).
+:- dynamic(sight_range/2).
+:- multifile(sight_range/2).
+:- dynamic(inventory/2).
+:- multifile(inventory/2).
+:- dynamic(btree_maze/4).
+:- multifile(btree_maze/4).
 
-:- dynamic inventory_cursor/2.
-:- multifile inventory_cursor/2.
+:- dynamic(inventory_cursor/2).
+:- multifile(inventory_cursor/2).
 
-:- dynamic on_get/1.
-:- multifile on_get/1.
+:- dynamic(on_get/1).
+:- multifile(on_get/1).
 
-:- dynamic status_msg/1.
-:- dynamic current_action/1.
+:- dynamic(status_msg/1).
+:- dynamic(current_action/1).
 
-:- multifile gen_room/3.
-:- multifile put_ent/2.
+:- multifile(gen_room/3).
+:- multifile(put_ent/2).
 
-:- multifile turn_pass/1.
-:- multifile wall/5.
-:- multifile is_container/2.
+:- multifile(turn_pass/1).
+:- multifile(wall/5).
+:- multifile(is_container/2).
