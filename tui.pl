e20c90600296accf5971cc9771484cba0bac0463
flan <flan@flande.re>
2021-11-13 23:55:54 +0100

    Make get_escape/2 use list_length/2

--- tui.pl
+++ tui.pl
@@ -3,7 +3,7 @@ get_key(K) :-
 	KC \= 27 -> K = [KC]; get_escape([27], K).
 
 % FIXME: escape sequences are assumed to be 3 characters long, always
-get_escape(L, E) :- length(L, 3), E = L, !.
+get_escape(L, E) :- list_length(L, 3), E = L, !.
 get_escape(L, E) :-
 	get_single_char(K),
 	append(L, [K], NE),
