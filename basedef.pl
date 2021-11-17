0d68da7c4445d342ba944de50bb3a2c97a1d65cd
flan <flan@flande.re>
2021-11-17 03:54:59 +0100

    Make all dynamic predicates multifile

--- basedef.pl
+++ basedef.pl
@@ -17,8 +17,13 @@
 :- dynamic(on_get/1).
 :- multifile(on_get/1).
 
+% not strictly multifile (these aren't defined anywhere, they're always
+% asserted at runtime) but gplc generates duplicate definitions without
+% specifying multifile
 :- dynamic(status_msg/1).
+:- multifile(status_msg/1).
 :- dynamic(current_action/1).
+:- multifile(current_action/1).
 
 :- multifile(gen_room/3).
 :- multifile(put_ent/2).
